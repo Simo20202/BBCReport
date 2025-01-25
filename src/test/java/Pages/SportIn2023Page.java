48349e589d8f7f87105a32705a60f2491d512db2
Thandazani <thandazanijrc@gmail.com>
2025-01-25 13:06:55 +0200

    Created method to click on Las  Vegas Grand Prix result and verify results table is displayed

--- src/test/java/Pages/SportIn2023Page.java
+++ src/test/java/Pages/SportIn2023Page.java
@@ -3,6 +3,10 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SportIn2023Page {
     WebDriver driver;
@@ -10,13 +14,26 @@ public class SportIn2023Page {
     @FindBy(xpath = "(//div[contains(.,'Abu Dhabi Grand Prix, Yas MarinaAbu Dhabi Grand Prix, Yas Marina,26 November 2023')])[11]")
     WebElement AbuDhabiGrandPrixYasMarinaLink_xpath;
 
+    @FindBy(xpath = "//span[@data-js-text='true'][contains(.,'Las Vegas Grand Prix, Las Vegas Street Circuit')]")
+    WebElement LasVegasGrandPrixResults_xpath;
+
+    @FindBy(xpath = "(//th[contains(@class,'ssrcss-1jdu2ly-StyledTableHeading ep4g78u0')])[2]")
+    WebElement resultsTable_xpath;
+
     public SportIn2023Page(WebDriver driver) {
         this.driver = driver;
     }
 
         public void clickAbuDhabiGrandPrixYasMarina() {
             AbuDhabiGrandPrixYasMarinaLink_xpath.click();
+        }
 
+        public void clickLasVegasGrandPrixResults() {
+            LasVegasGrandPrixResults_xpath.click();
+        }
 
+        public void verifyVisibilityOfResultsTable(){
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(resultsTable_xpath));
+            resultsTable_xpath.isDisplayed();
         }
     }
--- src/test/java/StepsDefinition/Base.java
+++ src/test/java/StepsDefinition/Base.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 public class Base {
 
    BrowserFactory browserFactory = new BrowserFactory();
-   final WebDriver driver = browserFactory.startBrowser("edge","https://www.bbc.com/sport");
+   final WebDriver driver = browserFactory.startBrowser("chrome","https://www.bbc.com/sport");
 
    BBCLandingPage bbcLandingPage = PageFactory.initElements(driver, BBCLandingPage.class);
    SportIn2023Page sportIn2023PagePage = PageFactory.initElements(driver, SportIn2023Page.class);
