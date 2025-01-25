7ca06a72b9bf4748dc0e956ba4fbdc989fde3dca
Thandazani <thandazanijrc@gmail.com>
2025-01-25 13:06:55 +0200

    Added a method to verify first position driver

--- src/test/java/Pages/SportIn2023Page.java
+++ src/test/java/Pages/SportIn2023Page.java
@@ -1,8 +1,15 @@
 package Pages;
 
+import io.cucumber.java.AfterAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class SportIn2023Page {
     WebDriver driver;
@@ -10,13 +17,50 @@ public class SportIn2023Page {
     @FindBy(xpath = "(//div[contains(.,'Abu Dhabi Grand Prix, Yas MarinaAbu Dhabi Grand Prix, Yas Marina,26 November 2023')])[11]")
     WebElement AbuDhabiGrandPrixYasMarinaLink_xpath;
 
+    @FindBy(xpath = "//span[@data-js-text='true'][contains(.,'Las Vegas Grand Prix, Las Vegas Street Circuit')]")
+    WebElement LasVegasGrandPrixResults_xpath;
+
+    @FindBy(xpath = "(//th[contains(@class,'ssrcss-1jdu2ly-StyledTableHeading ep4g78u0')])[2]")
+    WebElement resultsTable_xpath;
+
+    @FindBy(xpath = "(//span[@aria-hidden='true'][contains(.,'1')])[12]")
+    public WebElement TablePosition_xpath;
+
+    @FindBy(xpath = "(//span[@class='ssrcss-1hf3wfc-FullName e1dzfgvv0'][contains(.,'Max Verstappen')])[2]")
+    public WebElement DriverName_xpath;
+
+
+
+
+
     public SportIn2023Page(WebDriver driver) {
         this.driver = driver;
     }
 
         public void clickAbuDhabiGrandPrixYasMarina() {
             AbuDhabiGrandPrixYasMarinaLink_xpath.click();
-
-
         }
-    }
+
+        public void clickLasVegasGrandPrixResults() {
+            LasVegasGrandPrixResults_xpath.click();
+        }
+
+        public void verifyVisibilityOfResultsTable(){
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(resultsTable_xpath));
+            resultsTable_xpath.isDisplayed();
+        }
+
+        //added by Sandile
+         public boolean VerifyFirstPositionDriver(){
+          return ((TablePosition_xpath.getText().equals("1")) && (DriverName_xpath.getText().equalsIgnoreCase("Max Verstappen")));
+
+
+
+
+
+          }
+
+
+
+
+         }
--- src/test/java/StepsDefinition/Steps.java
+++ src/test/java/StepsDefinition/Steps.java
@@ -9,6 +9,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
 public class Steps extends Base {
 
@@ -17,56 +20,72 @@ public class Steps extends Base {
     public void the_user_is_in_the_bbc_landing_page() {
         bbcLandingPage.verifyThatBBCIsDisplayed();
     }
+
     @And("The user has selected the Formula one")
     public void the_user_has_selected_the_formula_one() {
         bbcLandingPage.clickFormula1();
     }
+
     @And("The user has selected the Results")
     public void the_user_has_selected_the_results() {
         bbcLandingPage.clickResults();
     }
+
     @And("The user has selected the year to report on")
     public void the_user_has_selected_the_year_to_report_on() throws InterruptedException {
 
         bbcLandingPage.selectYear("2023");
         Thread.sleep(5000);
     }
+
     @And("The user collapse the currently opened results")
     public void the_user_collapse_the_currently_opened_results() {
         sportIn2023PagePage.clickAbuDhabiGrandPrixYasMarina();
-//      Tlhogi
     }
+
     @When("The user click on the Las Vegas Grand Prix, Las Vegas Street Circuit")
-    public void the_user_click_on_the_las_vegas_grand_prix_las_vegas_street_circuit() {
-//      T-MAN
+    public void the_user_click_on_the_las_vegas_grand_prix_las_vegas_street_circuit() throws InterruptedException {
+        sportIn2023PagePage.clickLasVegasGrandPrixResults();
+        Thread.sleep(5000);
     }
+
     @Then("The Results table should be displayed")
-    public void the_results_table_should_be_displayed() {
-//      T-MAN
+    public void the_results_table_should_be_displayed() throws InterruptedException {
+        sportIn2023PagePage.verifyVisibilityOfResultsTable();
+
     }
+
     @And("The user validate that Max Verstappen took 1st place")
     public void the_user_validate_that_max_verstappen_took_1st_place() {
-//     Sandile
+        Assert.assertTrue(sportIn2023PagePage.VerifyFirstPositionDriver());
+
+
+
+
     }
+
     @And("The user validate that George Russell finished 2nd")
     public void the_user_validate_that_george_russell_finished_2nd() {
 //    Papas
     }
+
     @And("The user validate that Sergio Perez secured 3rd place")
     public void the_user_validate_that_sergio_perez_secured_3rd_place() {
 //      Mamo
     }
+
     @AfterStep
-    public void addScreenshot(Scenario scenario){
-        if (scenario.isFailed()){
-            byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot,"image/png","image");
+    public void addScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "image");
         }
 
     }
 
-    @After
-    public void closeBrowser(){
+
+   @After
+    public void closeBrowser() {
         driver.quit();
     }
 }
